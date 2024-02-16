8b615aa410557b616b53098b007a000265eb290e
engineergirly <girlytambeagbor@gmail.com>
2024-02-16 04:02:21 -0800

    completed geolocation functionality on a click event. Created an api fetch func using lat and lon from geolocation obj. Few modifications to style using js to add and remove classes

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // work on get device location button, will need to retrieve lon and lat cordinates for api fetch
 // start working on displaying weather info after a valid city has been entered 
-// 
+
 
 const wrapper = document.querySelector(".wrapper"),
 inputPart = wrapper.querySelector(".input-part"),
-infoTxt = wrapper.querySelector(".info-txt"),
-inputField = wrapper.querySelector("input");
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button");
+
+let url = '';
+
 
 inputField.addEventListener("keyup", e => {
     // if user presses enter and the input field is not empty
@@ -14,8 +18,36 @@ inputField.addEventListener("keyup", e => {
     }
 });
 
+locationBtn.addEventListener("click", ()=> {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }
+    else {
+        alert("Your browser does not support geolocation api")
+    }
+
+})
+
+function onError(error) {
+    infoTxt.innerText = error.message;
+    infoTxt.classList.add("error")
+}
+
+function onSuccess(position) {
+    //getting longitude and lat of user device from coords object
+    console.log(position)
+    const {latitude, longitude} = position.coords;
+    url = `https://api.weatherapi.com/v1/current.json?key=48652c7f4b124a759e0103923241402&q=${latitude},${longitude}&aqi=no`;
+    fetchData();
+}
+
+
 function requestApi(city) {
-    let url = `https://api.weatherapi.com/v1/current.json?key=48652c7f4b124a759e0103923241402&q=${city}&aqi=yes`;
+    url = `https://api.weatherapi.com/v1/current.json?key=48652c7f4b124a759e0103923241402&q=${city}&aqi=yes`;
+    fetchData();
+}
+
+function fetchData() {
     infoTxt.innerText = "Getting weather details.."
     infoTxt.classList.add("pending")
     
@@ -30,7 +62,32 @@ function requestApi(city) {
 }
 
 function weatherDetails(info) {
-    console.log(info)
+    if (info.error) {
+        infoTxt.classList.replace("pending", "error")
+        infoTxt.innerText = info.error.message;
+    }
+    else {
+       /* // grabbing values from api weather object
+        const city = info.location.name;
+        const country = info.location.country;
+        const description = info.current.condition[2];
+        const feelsLike = info.current.feelslike_f;
+        const humidity = info.current.humidity;
+        const temp = info.current.temp_f;
+
+        // setting above values to the corresponding html elements
+        wrapper.querySelector(".number") = temp;
+        wrapper.querySelector(".location span") = city;
+        // wrapper.querySelector("") = country;
+        wrapper.querySelector(".weather") = description;
+        wrapper.querySelector(".number-2") = feelsLike;
+        wrapper.querySelector(".humidity.details span") = humidity; **/
+
+        infoTxt.classList.remove("pending", "error");
+        wrapper.classList.add("active");
+
+        console.log(info)
+    }
 }
 
 
