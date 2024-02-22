c7e72268a60e5ddcec3ae3d7aadce5d0c3e26206
engineergirly <girlytambeagbor@gmail.com>
2024-02-22 03:55:07 -0800

    completed functionlity for weather icons

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ const wrapper = document.querySelector(".wrapper"),
 inputPart = wrapper.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
 inputField = inputPart.querySelector("input"),
+arrow = wrapper.querySelector("header i"),
 locationBtn = inputPart.querySelector("button");
 
 let url = '';
 
+arrow.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+});
 
 inputField.addEventListener("keyup", e => {
     // if user presses enter and the input field is not empty
@@ -27,7 +31,7 @@ locationBtn.addEventListener("click", ()=> {
         alert("Your browser does not support geolocation api")
     }
 
-})
+});
 
 function onError(error) {
     infoTxt.innerText = error.message;
@@ -83,6 +87,24 @@ function weatherDetails(info) {
         wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
         wrapper.querySelector(".number").innerText = temp; 
 
+        if (info.current.condition.code === 1000) {
+            wrapper.querySelector('img').src = 'Weather Icons/clear.svg'
+        }
+        else if (info.current.condition.code === 1003 || info.current.condition.code === 1006 || info.current.condition.code === 1009) {
+            wrapper.querySelector('img').src = 'Weather Icons/cloud.svg'
+        }
+        else if (info.current.condition.code === 1135) {
+            wrapper.querySelector('img').src = 'Weather Icons/haze.svg'
+        }
+        else if (info.current.condition.code === 1030 || info.current.condition.code === 1063 || info.current.condition.code === 1072 || info.current.condition.code === 1150 || info.current.condition.code === 1180 || info.current.condition.code === 1153 || info.current.condition.code === 1183 || info.current.condition.code === 1186 || info.current.condition.code === 1189 || info.current.condition.code === 1192 || info.current.condition.code === 1195 || info.current.condition.code === 1240 || info.current.condition.code === 1243 || info.current.condition.code === 1246) {
+            wrapper.querySelector('img').src = 'Weather Icons/rain.svg'
+        }
+        else if (info.current.condition.code === 1066 || info.current.condition.code === 1069 || info.current.condition.code === 1114 || info.current.condition.code === 1117 || info.current.condition.code === 1147 || info.current.condition.code === 1168 || info.current.condition.code === 1171 || info.current.condition.code === 1198 || info.current.condition.code === 1201 || info.current.condition.code === 1204 || info.current.condition.code === 1207 || info.current.condition.code === 1210 || info.current.condition.code === 1213 || info.current.condition.code === 1216 || info.current.condition.code === 1219 || info.current.condition.code === 1222 || info.current.condition.code === 1225 || info.current.condition.code === 1237 || info.current.condition.code === 1249 || info.current.condition.code === 1252 || info.current.condition.code === 1255 || info.current.condition.code === 1258 || info.current.condition.code === 1261 || info.current.condition.code === 1264) {
+            wrapper.querySelector('img').src = 'Weather Icons/snow.svg'
+        }
+        else {
+            wrapper.querySelector('img').src = 'Weather Icons/storm.svg'
+        }
 
         infoTxt.classList.remove("pending", "error");
         wrapper.classList.add("active");
@@ -90,7 +112,3 @@ function weatherDetails(info) {
         console.log(info)
     }
 }
-
-
-
-
